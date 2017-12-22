ca86ff4a5683b0f50c8db82a2c2ceab1a80723bf
varalakshmilokesh <varalakshmip12@gmail.com>
2017-12-21 22:55:44 -0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+<script>
+	$('.chart-1').percentcircle({
+ 		animate : true,
+ 		diameter : 100,
+		guage: 2,
+		coverBg: '#fff',
+		bgColor: '#efefef',
+		fillColor: '#5c93c8',
+		percentSize: '15px',
+		percentWeight: 'normal'
+});
+	$('.chart-2').percentcircle({
+ 		animate : true,
+ 		diameter : 100,
+		guage: 2,
+		coverBg: '#fff',
+		bgColor: '#efefef',
+		fillColor: '#5c93c8',
+		percentSize: '15px',
+		percentWeight: 'normal'
+});
+	$('.chart-3').percentcircle({
+ 		animate : true,
+ 		diameter : 100,
+		guage: 2,
+		coverBg: '#fff',
+		bgColor: '#efefef',
+		fillColor: '#5c93c8',
+		percentSize: '15px',
+		percentWeight: 'normal'
+});
+</script>
